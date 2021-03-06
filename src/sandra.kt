f3dddf62d6ebb78e28ed94b75eee16aeb5cd78e4
Sandra Nasasiira <sandranasasiiraresty@gmail.com>
2021-03-06 08:40:08 +0300

    first commit

---
+++ src/sandra.kt
@@ -0,0 +1,29 @@
+fun main() {
+    name()
+    modulus()
+    add()
+    me()
+}
+fun name(){
+    var name=("sandra")
+    println(name)
+}
+fun modulus(){
+    var num1=4
+    var num2=3
+    var modulus=(num1%num2)
+    println(modulus)
+}
+fun add(){
+    var num3=3
+    var num4=5
+    var num5=7
+    var num6=9
+    var add=(num3+num4+num5+num6)
+    println(add)
+
+}
+fun me(){
+    var me=("strong")
+    println(me)
+}
